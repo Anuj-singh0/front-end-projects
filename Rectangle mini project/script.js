882fd52f6e915d3f170720338adf646604a2e052
Anuj Singh <anujsingh454568@gmail.com>
2025-01-17 02:59:12 +0530

    small changes

--- Rectangle mini project/script.js
+++ Rectangle mini project/script.js
@@ -6,27 +6,28 @@ rect.addEventListener("mousemove", function (details) {
 
     if (insiderectval < rectanglelocation.width / 2) {
         var redcolor = gsap.utils.mapRange(
-            0, 
-            rectanglelocation.width / 2, 
-            255, 
-            0, 
+            0,
+            rectanglelocation.width / 2,
+            255,
+            0,
             insiderectval
         );
         gsap.to(rect, {
-            backgroundColor: `rgb(${redcolor},0,0)`, 
+            backgroundColor: `rgb(${redcolor},0,0)`,
             ease: Power4,
         });
 
     } else {
         var bluecolor = gsap.utils.mapRange(
-            rectanglelocation.width / 2, 
+            rectanglelocation.width / 2,
             rectanglelocation.width,
             0,
-            255, 
+            255,
             insiderectval
         );
         gsap.to(rect, {
-            backgroundColor: `rgb(0,0,${bluecolor})`, ease: Power4,
+            backgroundColor: `rgb(0,0,${bluecolor})`, 
+            ease: Power4,
         });
 
     }
