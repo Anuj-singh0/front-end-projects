beef5c65da874b65f511bff52e934ac61097272f
Anuj Singh <anujsingh454568@gmail.com>
2025-01-21 08:52:05 +0530

    enhance mousemove effect to include image display with GSAP animations; update styles for image positioning

--- web effects using JavaScript/script.js
+++ web effects using JavaScript/script.js
@@ -8,18 +8,36 @@ const throttleFunction = (func, delay) => {
         }
     };
 };
-document.querySelector('#center')
-    .addEventListener(
-        "mousemove",
-        throttleFunction((dets) => {
-            var div = document.createElement("div");
-            div.classList.add("imagediv");
-            div.style.left = dets.clientX + 'px';
-            div.style.top = dets.clientY + 'px';
 
-            document.body.appendChild(div);
-            setTimeout(function () {
-                div.remove();
-            }, 2000);
-        }, 400)
-    );
+const centerElement = document.querySelector('#center');
+
+centerElement.addEventListener(
+    "mousemove",
+    throttleFunction((dets) => {
+        const div = document.createElement("div");
+        div.classList.add("imagediv");
+        div.style.left = `${dets.clientX}px`;
+        div.style.top = `${dets.clientY}px`;
+
+        const img = document.createElement("img");
+        img.setAttribute("src", "https://static.vecteezy.com/system/resources/thumbnails/041/166/062/small/ai-generated-hawk-high-quality-image-free-photo.jpg");
+        div.appendChild(img);
+
+        document.body.appendChild(div);
+
+        const tl = gsap.timeline();
+        tl.to(img, {
+            y: "0",
+            ease: Power1.easeOut,
+            duration: 0.6
+        }).to(img, {
+            y: "100%",
+            ease: Power2.easeOut,
+            delay: 0.6
+        });
+
+        setTimeout(() => {
+            div.remove();
+        }, 2000);
+    }, 400)
+);
