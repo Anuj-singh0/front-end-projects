2302073da01ee95cc13d39110109028215106b5b
Anuj Singh <anujsingh454568@gmail.com>
2025-01-21 08:10:53 +0530

    enhance mousemove effect to position div and auto-remove after 2 seconds; add overflow hidden to prevent scroll

--- web effects using JavaScript/script.js
+++ web effects using JavaScript/script.js
@@ -7,14 +7,19 @@ const throttleFunction = (func, delay) => {
             return func(...args);
         }
     };
-}
+};
 document.querySelector('#center')
-.addEventListener(
-    "mousemove",
-    throttleFunction((e) => {
-        var div = document.createElement("div");
-        div.classList.add("imagediv");
-        document.body.appendChild(div);
-    }, 400)
+    .addEventListener(
+        "mousemove",
+        throttleFunction((dets) => {
+            var div = document.createElement("div");
+            div.classList.add("imagediv");
+            div.style.left = dets.clientX + 'px';
+            div.style.top = dets.clientY + 'px';
 
-)
+            document.body.appendChild(div);
+            setTimeout(function () {
+                div.remove();
+            }, 2000);
+        }, 400)
+    );
